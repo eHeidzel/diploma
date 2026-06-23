7f318081a439ddd020af48053f96f10b251eab32
eHeidzel <e.heidzel@gmail.com>
2026-06-23 12:50:38 +0300

    test

--- api/index.js
+++ api/index.js
@@ -1,33 +1,6 @@
-// api/index.js
-const { NestFactory } = require('@nestjs/core');
-const { AppModule } = require('../diploma/backend/dist/modules/app.module');
-const { ValidationPipe } = require('@nestjs/common');
-const { join } = require('path');
+import { getApp } from '../src/main';
 
-let app;
-
-module.exports = async (req, res) => {
-  if (!app) {
-    app = await NestFactory.create(AppModule);
-    
-    app.useGlobalPipes(
-      new ValidationPipe({
-        whitelist: true,
-        transform: true,
-      })
-    );
-
-    app.enableCors({
-      origin: true,
-      credentials: true,
-      methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
-      allowedHeaders: ['Content-Type', 'Authorization', 'Accept', 'X-Requested-With'],
-    });
-    
-    await app.init();
-  }
-  
-  // Перенаправляем запрос в NestJS
-  const expressApp = app.getHttpAdapter().getInstance();
-  expressApp(req, res);
+export default async (req, res) => {
+  const app = await getApp();
+  app(req, res);
 };
